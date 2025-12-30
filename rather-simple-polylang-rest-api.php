4958172ad381f00453424018cd0d9231b6d0ce34
leemon <tacrapo@gmail.com>
2025-12-30 16:07:44 +0100

    Fix version

--- rather-simple-polylang-rest-api.php
+++ rather-simple-polylang-rest-api.php
@@ -3,7 +3,7 @@
  * Plugin Name: Rather Simple Polylang REST API
  * Plugin URI:
  * Update URI: false
- * Version: 3.0
+ * Version: 1.0
  * Requires at least: 6.8
  * Requires PHP: 7.4
  * Requires Plugins: polylang
