cd3a3ddd5ef5e684dfe4792b021bacb2341d56d3
leemon <tacrapo@gmail.com>
2026-01-17 23:02:22 +0100

    Improve icon slightly

--- build/blocks/language-switcher/index.asset.php
+++ build/blocks/language-switcher/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'c9fdd7693d029bb0d8cb');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '609443448425b6531836');
