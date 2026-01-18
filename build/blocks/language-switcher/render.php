3ce811930e3dda24d7f2dcba80979d50f3bf7d1c
leemon <tacrapo@gmail.com>
2026-01-18 21:41:11 +0100

    Change package name

--- build/blocks/language-switcher/render.php
+++ build/blocks/language-switcher/render.php
@@ -6,7 +6,7 @@
  * @param string   $content        Rendered block output. ie. <InnerBlocks.Content />.
  * @param WP_Block $block          The instance of the WP_Block class that represents the block being rendered.
  *
- * @package jn_core
+ * @package rather_simple_polylang_free_extras
  */
 
 ?>
