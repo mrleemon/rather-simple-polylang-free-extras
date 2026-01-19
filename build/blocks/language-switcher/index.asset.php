f9d2c005d556deec95d71425009b44c6221f27dd
leemon <tacrapo@gmail.com>
2026-01-20 00:59:01 +0100

    Fix condition

--- build/blocks/language-switcher/index.asset.php
+++ build/blocks/language-switcher/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '691fc06ce2460e933899');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '0bb08c327c6f508a26c8');
